8cc033e6c9716a86a7a32ff6fc92accba48253ca
cbuchwald229 <31973954+cbuchwald229@users.noreply.github.com>
2018-11-21 19:53:12 -0600

    Password encryption

--- EmployeeManager.Web/EmployeeManager.Web/Controllers/LoginController.cs
+++ EmployeeManager.Web/EmployeeManager.Web/Controllers/LoginController.cs
@@ -1,5 +1,8 @@
 ﻿using EmployeeManager.Web.Models;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web.Mvc;
 
 namespace EmployeeManager.Web.Controllers
@@ -16,8 +19,18 @@ namespace EmployeeManager.Web.Controllers
         {
             using (EmployeeManagerEntities db = new EmployeeManagerEntities())
             {
+                if (userModel.password != null)
+                {
+                    var encryptedPassword = EncryptString(userModel.password);
+                }
+                else
+                {
+                    userModel.LoginErrorMessage = "Wrong Username or Password.";
+                    return View("Index", userModel);
+                }
+
                 var userDetails = db.Users.Where(x => x.username == userModel.username && x.password == userModel.password).FirstOrDefault();
-                if(userDetails == null)
+                if (userDetails == null)
                 {
                     userModel.LoginErrorMessage = "Wrong Username or Password.";
                     return View("Index", userModel);
@@ -36,6 +49,62 @@ namespace EmployeeManager.Web.Controllers
             int userId = (int)Session["userId"];
             Session.Abandon();
             return RedirectToAction("Index", "Login");
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+        public string EncryptString(string encryptString)
+        {
+            string EncryptionKey = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            byte[] clearBytes = System.Text.Encoding.Unicode.GetBytes(encryptString);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] {
+            0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76
+        });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
+                    }
+                    encryptString = System.Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return encryptString;
+        }
+
+        public string Decrypt(string cipherText)
+        {
+            string EncryptionKey = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            cipherText = cipherText.Replace(" ", "+");
+            byte[] cipherBytes = System.Convert.FromBase64String(cipherText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] {
+            0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76
+        });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(cipherBytes, 0, cipherBytes.Length);
+                        cs.Close();
+                    }
+                    cipherText = Encoding.Unicode.GetString(ms.ToArray());
+                }
+            }
+            return cipherText;
+
         }
     }
 }
